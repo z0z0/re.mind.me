5920638362a745e00d1fad81cf4be6719903f71e
gimlet <andrey.chernishov@gmail.com>
2013-12-08 12:07:43 +0100

    phonegap branche added

---
+++ www/platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/gimlet/workspace/phonegap/notify.me/app/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
+/home/gimlet/workspace/phonegap/notify.me/app/platforms/android/bin/HelloWorld.ap_ \
+/home/gimlet/workspace/phonegap/notify.me/app/platforms/android/bin/classes.dex \
+/home/gimlet/workspace/phonegap/notify.me/app/platforms/android/libs/cordova-3.2.0.jar \
